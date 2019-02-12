0f175d93867cbe64dba3dcc67f8bc82951a07dc5
Rashid Goshtasbi <Rashid Goshtasbi>
2019-02-11 23:39:57 -0800

    code cleanup

--- src/main/scala/com/swayapp/search/Main.scala
+++ src/main/scala/com/swayapp/search/Main.scala
@@ -1,8 +1,8 @@
 package com.swayapp.search
 
 
-import java.nio.charset.Charset
 import java.util._
+import spray.json._
 
 import com.amazonaws.ClientConfiguration
 import com.amazonaws.regions.Regions
@@ -11,17 +11,13 @@ import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.{APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent, KinesisEvent}
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.document.DynamoDB
-import spray.json._
-import DefaultJsonProtocol._
 import com.amazonaws.retry.PredefinedBackoffStrategies.ExponentialBackoffStrategy
 import com.amazonaws.retry.RetryPolicy
-import com.amazonaws.services.dynamodbv2.document.spec.{GetItemSpec, QuerySpec, ScanSpec}
-import com.amazonaws.services.dynamodbv2.model.ScanRequest // if you don't supply your own Protocol (see below)
+import com.amazonaws.services.dynamodbv2.model.ScanRequest
 
 
 class Main extends RequestHandler[Map[String, Any], APIGatewayProxyResponseEvent] {
 
-//  override def handleRequest(event: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent = {
   override def handleRequest(event: Map[String, Any], context: Context): APIGatewayProxyResponseEvent = {
     val retryPolicy = new RetryPolicy(
       null,
@@ -43,12 +39,12 @@ class Main extends RequestHandler[Map[String, Any], APIGatewayProxyResponseEvent
     val requestBody = event
     println("request body: " + requestBody.get("body").asInstanceOf[Map[String, String]])
 
-//    val spec = new ScanRequest()
-//      .withTableName("INFLUENCER_INFO")
-//    val scanResults = dynamoClient.scan(spec)
-//    scanResults.getItems.forEach(record => {
-//      println("record: " + record.toString)
-//    })
+    val spec = new ScanRequest()
+      .withTableName("INFLUENCER_INFO")
+    val scanResults = dynamoClient.scan(spec)
+    scanResults.getItems.forEach(record => {
+      println("record: " + record.toString)
+    })
 
     new APIGatewayProxyResponseEvent().withBody("works").withStatusCode(200)
   }
